3ee33c16181c4b1dd15f4184ddb80ef63ab45ddf
Brianvogt <Brian.Vogt@Kellogg.Northwestern.edu>
2020-05-28 21:46:55 +0000

    any_usernumber connected

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,8 @@ def photo_index
   render({:template => "photo_templates/photo_index.html.erb"})
   
 end
+
+def photo_details 
+  render({:template => "photo_templates/photo_detail.html.erb"})
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  get("/photos", {:controller => "photos", :action => "photo_index"}) 
+ get("/photos/:any_usernumber", {:controller => "photos", :action => "photo_details"})
 end
