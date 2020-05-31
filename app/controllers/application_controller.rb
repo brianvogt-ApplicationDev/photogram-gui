5131cf95512a88ce5c03dac654370482114aaa14
Brianvogt <Brian.Vogt@Kellogg.Northwestern.edu>
2020-05-31 22:15:48 +0000

    Index is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def user_index
+  @all_users = User.all.order({ :created_at => :desc })
+
+  render({:template => "photo_templates/users_index.html.erb"})
+end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- get("/users", {:controller => "users", :action => "user_index"}) 
+ get("/users", {:controller => "application", :action => "user_index"}) 
+ 
 
  get("/photos", {:controller => "photos", :action => "photo_index"}) 
  get("/photos/:any_photonumber", {:controller => "photos", :action => "photo_details"})
