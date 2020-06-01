53f4ce2a016f9e4f86f7e5695d5994637afba11f
Brianvogt <Brian.Vogt@Kellogg.Northwestern.edu>
2020-06-01 21:11:45 +0000

    create username connected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   render({:template => "photo_templates/users_index.html.erb"})
 end  
 
+def new_user
+  render({:template => "photo_templates/user_new.html.erb"})
+end  
+
 def photo_index
    @all_photo = Photo.all.order({ :created_at => :desc })
    
@@ -25,5 +29,14 @@ end
      
 
     render({ :template => "photo_templates/user_detail.html.erb"})
+  end
+
+  def photo_details
+    the_p = params.fetch("any_photonumber")
+    mathes = Photo.where({ :id => the_p })
+    @photo = mathes.at(0)
+    render({ :template => "photo_templates/photo_detail.html.erb"})
+
+  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
  get("/users", {:controller => "application", :action => "user_index"}) 
+ get("/insert_user_record", {:controller => "application", :action => "new_user"})
+
  get("/users/:any_username", {:controller => "application", :action => "user_details"})
+
+
+ 
 
  
  
  
 
  get("/photos", {:controller => "application", :action => "photo_index"})
-
- get("/photos/:any_photonumber", {:controller => "photos", :action => "photo_details"})
+  get("/photos/:any_photonumber", {:controller => "application", :action => "photo_details"})
 end
